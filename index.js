50e706721023da09bcc5e997f4f7dfe62f3ee1ee
Daybrush <daybrush@gmail.com>
2019-03-19 02:11:13 +0900

    fix recorder

--- index.js
+++ index.js
@@ -70,25 +70,8 @@ function rmdir(path) {
       });
       fs.rmdirSync(path);
     }
-  };
-async function convertAudio(path, startTime, endTime) {
-    console.log("Convert Audio", path);
-    return new Promise((resolve, reject) => {
-        ffmpeg(path)
-        .seekInput(startTime)
-        .inputOptions(`-to ${endTime}`)
-        // .audioCodec('aac')
-        // .audioBitrate('128k')
-        .on('error', function(err) {
-            console.log('An error occurred: ' + err.message);
-            reject();
-        })
-        .on('end', function() {
-            resolve();
-        })
-        .save("./.scene_cache/" + path);
-    });
-}
+};
+
 async function recordVideo({
     duration,
     fps,
@@ -103,9 +86,10 @@ async function recordVideo({
             frames[i] = `./.scene_cache/frame${i}.png`;
         }
 
-        await Promise.all(audios.map(audio => {
-            return convertAudio(audio[0], audio[3], audio[4]);
+        await Promise.all(audios.map(([path, delay, startTime, endTime], i) => {
+            return convertAudio(i, path, delay, startTime, endTime);
         }));
+        await mergeAudios(audios.length);
 
         console.log(`Processing start (totalframe: ${frames.length}, duration: ${duration}, fps: ${fps})`);
         const converter = ffmpeg()
@@ -126,36 +110,21 @@ async function recordVideo({
             .outputOption('-pix_fmt yuv420p')
             .size(`${width}x${height}`)
             .format('mp4')
-
-        // audios.forEach(audio => {
-        //     converter.addInput("./.scene_cache/" + audio[0])
-        //     // .audioFilters(["adelay=5000|5000"])
-        // })
-            converter.addInput("concat:./.scene_cache/test.mp3|./.scene_cache/test2.mp3");
-            converter.audioCodec('aac')
+            .addInput("./.scene_cache/merge.mp3")
+            .audioCodec('aac')
             .audioBitrate('128k')
             // .audioFrequency(22050)
             .audioChannels(4)
             .save(output);
     });
 }
-async function convertAudio2(path, startTime, endTime) {
+async function convertAudio(i, path, delay, startTime, endTime) {
     console.log("Convert Audio", path);
     return new Promise((resolve, reject) => {
-        ffmpeg()
-        .addInput("./.scene_cache/test.mp3")
-        // .audioFilters(["adelay=2000|2000"])
-        .addInput("./.scene_cache/test2.mp3")
-        .inputOptions(`-filter_complex amerge`)
-        // .audioFilters([
-        //     "amerge",
-        //     // "amovie=./.scene_cache/test.mp3 [l]",
-        //     // "amovie=./.scene_cache/test2.mp3 [r]",
-        //     // "[l] [r] amerge",
-        // ])
-        // .inputOptions(`-to ${endTime}`)
-        // .audioCodec('aac')
-        // .audioBitrate('128k')
+        ffmpeg(path)
+        .seekInput(startTime)
+        .inputOptions(`-to ${endTime}`)
+        .audioFilters([`adelay=${delay * 1000}|${delay * 1000}`, `atempo=2`])
         .on('error', function(err) {
             console.log('An error occurred: ' + err.message);
             reject();
@@ -163,7 +132,26 @@ async function convertAudio2(path, startTime, endTime) {
         .on('end', function() {
             resolve();
         })
-        .save("./.scene_cache/test3.mp3");
+        .save(`./.scene_cache/audio${i}.mp3`);
+    });
+}
+async function mergeAudios(count) {
+    console.log("Merge Audios");
+    return new Promise((resolve, reject) => {
+        const converter = ffmpeg();
+        for (let i = 0; i < count; ++i) {
+            converter.addInput(`./.scene_cache/audio${i}.mp3`);
+        }
+        
+        converter.inputOptions(`-filter_complex amix=inputs=${count}:duration=longest`)
+        .on('error', function(err) {
+            console.log('An error occurred: ' + err.message);
+            reject();
+        })
+        .on('end', function() {
+            resolve();
+        })
+        .save("./.scene_cache/merge.mp3");
     });
 }
 (async () => {
@@ -174,32 +162,30 @@ async function convertAudio2(path, startTime, endTime) {
     const height = 1080;
     const output = "output.mp4";
     const startTime = 0;
+    const audios = [
+        ["./test.mp3", 0, 20, 25],
+        ["./test2.mp3", 1, 40, 45],
+    ];
     let duration;
 
-    // const {
-    //     duration: sceneDuration,
-    // } = await caputreScene({
-    //     name,
-    //     path,
-    //     fps,
-    //     width,
-    //     height,
-    //     startTime,
-    //     duration
-    // });
-
-    await convertAudio2();
-    // await recordVideo({
-    //     duration: 12.857142857142857,
-    //     fps,
-    //     output,
-    //     width,
-    //     height,
-    //     audios: [
-    //         ["./test.mp3", 0, 10, 20, 25],
-    //         ["./test2.mp3", 0, 10, 40, 45],
-    //     ],
-    // });
-
-//    rmdir("./.scene_cache");
+    const {
+        duration: sceneDuration,
+    } = await caputreScene({
+        name,
+        path,
+        fps,
+        width,
+        height,
+        startTime,
+        duration
+    });
+    await recordVideo({
+        duration: sceneDuration,
+        fps,
+        output,
+        width,
+        height,
+        audios,
+    });
+   rmdir("./.scene_cache");
 })();
